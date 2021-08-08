599b6e7b6fe14809cd3523d01e8dad08d505449f
fnix-lu <fnix-lu@users.noreply.github.com>
2021-08-08 13:31:35 +0000

    deploy: 357a5ec14dc8b05abe58966cd783e8a808b80de7

---
+++ precache-manifest.ff1903de102e7cf89726020dcc9eba4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2874702525298a56c4146d334d995aa",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "cf2588009a1f98d8f1aa",
+    "url": "/resume/static/css/main.cdf2e4da.chunk.css"
+  },
+  {
+    "revision": "05c00ac4619033849455",
+    "url": "/resume/static/js/2.8a4ded50.chunk.js"
+  },
+  {
+    "revision": "8372c89b5681345eb53400d6379de718",
+    "url": "/resume/static/js/2.8a4ded50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf2588009a1f98d8f1aa",
+    "url": "/resume/static/js/main.19547c45.chunk.js"
+  },
+  {
+    "revision": "3054312907cc7b0d126a",
+    "url": "/resume/static/js/runtime-main.d32952cb.js"
+  }
+]);
